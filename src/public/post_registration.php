432bee0e464186b8eb2392deaccecca7ffae401f
ElizaF03 <elizafad03@gmail.com>
2024-04-18 21:30:22 +0800

    проверка при регистрации на уже существующий в БД емейл

--- src/public/post_registration.php
+++ src/public/post_registration.php
@@ -1,5 +1,5 @@
 <?php
-require_once './User.php';
+require_once '../Model/User.php';
 
 if(isset($_POST['name'])){
     $username = $_POST['name'];
@@ -13,22 +13,26 @@ if(isset($_POST['password'])){
 if(isset($_POST['repeat-password'])){
     $pswRepeat = $_POST['repeat-password'];
 }
-
+$user = new User();
 $errors=[];
 if(strlen($username) < 2){
     $errors['name']='Name is too short';
 }
 if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
     $errors['email']='Invalid email format';
-}
+}else{
+    if($user->getUserByEmail($email)){
+        $errors['email']='Email already exists';
+    }
 if(preg_match("/^[a-zA-Z0-9]+$/", $password) || strlen($password) < 6){
     $errors['password']='Password must contain as many as 6 characters including lower-case, upper-case, numbers and symbols.';
+}
 }
 if($pswRepeat != $password){
     $errors['pswRepeat']='Errors matching password';
 }
 if(empty($errors)){
-    $user = new User();
+
     $user->registration($username, $email, $password);
 }
 
